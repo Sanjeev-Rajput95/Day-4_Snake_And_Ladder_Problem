4f4e019ee00bdb582ff7e0f14e581160879cf1de
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-31 22:43:33 +0530

    Start position with 1 player

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------Welcome to the Snake and Ladder Program------- ");
+
+            Day4Problem day4ProblemObj = new Day4Problem();
+
+            day4ProblemObj.singlePlayer();
+        }
+    }
+}
