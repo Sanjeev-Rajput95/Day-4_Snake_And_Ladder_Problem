54a2917b0c41f63db58312d13cc45f14bd4a384d
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-01 23:38:06 +0530

    Playing till player reaches 100

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -7,11 +7,7 @@
             Console.WriteLine("-------Welcome to the Snake and Ladder Program------- ");
 
             Day4Problem day4ProblemObj = new Day4Problem();
-
-            //  day4ProblemObj.singlePlayer();
-
-            //  day4ProblemObj.firstDiceRoll();
-            day4ProblemObj.snakeAndLadder();
+            day4ProblemObj.winningPosition();
         }
     }
 }
